e600a24f948ad827e3da330ac9c9a52c07ffc4c8
Unknown <bryanparisot@hotmail.com>
2021-08-02 14:35:50 +0200

    :memo: add close modal in js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const formClose = document.getElementsByClassName("close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// launch modal form&
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+//close la fenêtre modal
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+formClose[0].addEventListener('click', closeModal);
+
 
 
